fa3ea6a6770341add903a7f6476d385e7ddeacb1
kumapo <kumapo@users.noreply.github.com>
2016-02-06 22:25:41 +0900

    Transit to new project file, refs #2

---
+++ ScrollableTabs/ScrollableTabs.h
@@ -0,0 +1,19 @@
+//
+//  ScrollableTabs.h
+//  ScrollableTabs
+//
+//  Created by kumapo on 2016/02/06.
+//  Copyright © 2016年 kumapo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ScrollableTabs.
+FOUNDATION_EXPORT double ScrollableTabsVersionNumber;
+
+//! Project version string for ScrollableTabs.
+FOUNDATION_EXPORT const unsigned char ScrollableTabsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScrollableTabs/PublicHeader.h>
+
+
